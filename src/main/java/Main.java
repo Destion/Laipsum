2cea1660211573f4a60efd9278e2a2f07fb8e330
Destion <mick.sneekes@live.nl>
2015-11-17 12:59:32 +0100

    added mian

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by destion on 17-11-15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Gerben is kut en Aede is fries.");
+    }
+
+}

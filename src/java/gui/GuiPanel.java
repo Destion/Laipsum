7506fafb1810b1c55828a65c14443336232829ab
Destion <m.sneekes@student.utwente.nl>
2016-01-04 21:54:23 +0100

    push

--- src/java/gui/GuiPanel.java
+++ src/java/gui/GuiPanel.java
@@ -1,15 +1,34 @@
 package gui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Destion on 4-1-2016.
  */
 public class GuiPanel extends JPanel {
+    private static final String IMG_PATH = "src/java/gui/knutselen-met-herfstbladeren.jpg";
 
     public GuiPanel(){
         super();
+        try {
+            BufferedImage img = ImageIO.read(new File(IMG_PATH));
+            ImageIcon icon = new ImageIcon(img);
+            JLabel label = new JLabel(icon);
+            JOptionPane.showMessageDialog(null, label);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    protected void paintComponent(Graphics g){
+        this.repaint();
+        super.paintComponent(g);
     }
 
 }

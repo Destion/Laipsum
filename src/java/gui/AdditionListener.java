5d8403cbad2767c8bea846514a67acd287ec46f4
Destion <m.sneekes@student.utwente.nl>
2016-01-05 21:41:44 +0100

    theorattically added implementation for adding values to certain classifiers

---
+++ src/java/gui/AdditionListener.java
@@ -0,0 +1,42 @@
+package gui;
+
+import classifiers.NaiveBayesClassifier;
+import data.NaiveBayesTrainingDataImplementation;
+import gui.mainScreenComponents.ClassSelectionComboBox;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Destion on 5-1-2016.
+ */
+public class AdditionListener implements ActionListener {
+
+    JTextField tf;
+    ClassSelectionComboBox cb;
+    NaiveBayesClassifier nbc;
+
+    public AdditionListener(JTextField tf, ClassSelectionComboBox cb, NaiveBayesClassifier nbc){
+        super();
+        this.tf = tf;
+        this.cb = cb;
+        this.nbc = nbc;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        char[] temp = tf.getText().toCharArray();
+        boolean valid = true;
+
+        if (tf.getText() == "" || cb.getSelectedItem() == null){
+            valid = false;
+        }
+
+        if (valid){
+            System.out.println(tf.getText());
+            System.out.println((String) cb.getSelectedItem());
+            nbc.train(tf.getText(), (String) cb.getSelectedItem());
+        }
+    }
+}
